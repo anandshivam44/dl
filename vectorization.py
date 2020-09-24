f9093942aa73bd030bfa29aea825bddb4ae0299b
anandshivam44 <anand.shivam44@yahoo.com>
2020-09-24 21:14:49 +0530

    first commit

---
+++ vectorization.py
@@ -0,0 +1,14 @@
+import numpy as np
+a=np.random.rand(1000000)
+b=np.random.rand(1000000)
+import time
+start=time.time()
+c=np.dot(a,b)
+end=time.time()
+print("Vectorized version: "+str(1000*(end-start)))
+c=0
+start=time.time()
+for i in range (1000000):
+    c+=a[i]*b[i]
+end=time.time()
+print("For loop: "+str(1000*(end-start)))
